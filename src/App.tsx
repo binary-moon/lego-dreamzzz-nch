e00b6544d4fba0a2cb03864ee5beaf76316c542b
Emre Dokur <emre.dokur@gmail.com>
2024-07-13 17:39:57 +1000

    fix eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 import "./App.css";
 import { Intro } from "./components/Intro";
 
 function App() {
-  const [gameState, setGameState] = useState(0);
+  const [gameState] = useState(0);
   return (
     <div className="flex flex-col w-full h-full absolute top-0 left-0 bg-background text-white overflow-hidden font-external">
       {gameState === 0 && <Intro />}
