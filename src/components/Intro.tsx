7638a557592c0f60356ac3807b277d9fb672e88c
Emre Dokur <edokur@seven.com.au>
2024-07-14 17:23:21 +1000

    adjust bg and dev server

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -10,6 +10,7 @@ import skullIsland from "../assets/images/skull-island.png";
 import islandWithDoor from "../assets/images/island-with-door.png";
 import legoCharacter from "../assets/images/lego-character.png";
 import theNeverWitch from "../assets/images/the-never-witch.png";
+import spiral from "../assets/images/spiral.png";
 
 import { Button } from "./Button";
 import { GlowingText } from "./GlowingText";
@@ -48,24 +49,15 @@ export const Intro = () => {
       <img
         src={logo}
         alt="Lego - Dreamzzz"
-        className="absolute top-[3vh] left-8 w-[60%] h-auto object-contain"
+        className="absolute top-[3dvh] left-8 w-[60%] h-auto object-contain"
       />
-      <motion.div
-        className="absolute bottom-0 left-0 w-screen h-[75vh] bg-cover"
+      <div
+        className="absolute bottom-0 left-0 w-screen h-[75dvh] bg-cover bg-right"
         style={{ backgroundImage: `url(${background})` }}
-        animate={{
-          backgroundPosition: ["0% 0%", "100% 0%"],
-        }}
-        transition={{
-          duration: 20,
-          ease: "linear",
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
       >
         <img
           src={rocks}
-          className="absolute bottom-0 -translate-y-[6vh] -translate-x-[0vw] w-[40vh] h-auto right-0 object-contain"
+          className="absolute bottom-0 -translate-y-[6dvh] -translate-x-[0dvw] w-[40dvh] h-auto right-0 object-contain"
           alt="rocks"
         />
         <img
@@ -75,39 +67,39 @@ export const Intro = () => {
         />
         <motion.img
           src={darkCharacters}
-          className="absolute top-[30vh] w-[25vh] h-auto -right-[19vw] object-contain"
+          className="absolute top-[30dvh] w-[25dvh] h-auto -right-[19dvw] object-contain"
           alt="Dark Characters"
           animate={subtleFloatingAnimation}
         />
         <img
           src={skullIsland}
-          className="absolute top-[30vh] -left-[9vw] w-[10vh] h-auto object-contain"
+          className="absolute top-[30dvh] -left-[9dvw] w-[10dvh] h-auto object-contain"
           alt="Skull Island"
         />
         <motion.img
           src={islandWithDoor}
-          className="absolute top-[40vh] -left-[0] w-[15vh] h-auto object-contain"
+          className="absolute top-[40dvh] -left-[0] w-[15dvh] h-auto object-contain"
           alt="Island With Door"
           animate={subtleWobblingAnimation}
         />
         <motion.img
           src={legoCharacter}
-          className="absolute top-[45vh] left-[7vw] w-[8vh] h-auto object-contain"
+          className="absolute top-[45dvh] left-[7dvw] w-[8dvh] h-auto object-contain"
           alt="Lego Character"
           animate={subtleWobblingAnimation}
         />
         <img
           src={darkRockIsland}
-          className="absolute bottom-0 right-0 w-[100vw] h-auto translate-y-[24vh] translate-x-[12vw] object-contain"
+          className="absolute bottom-0 right-0 w-[100dvw] h-auto translate-y-[24dvh] translate-x-[12dvw] object-contain"
           alt="Dark Rock Island"
         />
         <motion.img
           src={theNeverWitch}
-          className="absolute top-0 -right-[8vw] w-[100vh] h-auto object-contain"
+          className="absolute top-0 -right-[8dvw] w-[100dvh] h-auto object-contain"
           alt="The Never Witch"
           animate={floatingAnimation}
         />
-      </motion.div>
+      </div>
       <span className="flex flex-col items-end absolute right-8 top-[92px] -rotate-[8deg]">
         <span>
           <GlowingText text="Nightmare" />
@@ -121,7 +113,7 @@ export const Intro = () => {
       </span>
       <Button
         text="Start!"
-        className="absolute bottom-12 w-[66vw] -translate-x-[50%]"
+        className="absolute bottom-12 w-[66dvw] -translate-x-[50%]"
         onClick={() => console.log("Start Game")}
       />
     </div>
