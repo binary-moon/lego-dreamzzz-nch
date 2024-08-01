b86a112b7fbc9609aca14af976ab38e583910753
Declan Dwyer <declan@graviton.co>
2024-08-02 03:18:30 +1000

    Nightly Game Update.

--- public/game/__settings__.js
+++ public/game/__settings__.js
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'deviceTypes': [`webgl2`, `webgl1`],
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 188339330, 189724037, 188339296, 188339298, 188340596, 188351002, 188610540, 188610545, 188618901, 188619019, 188699334, 188860857, 188869861, 189021033, 189047757, 189279488, 189641079, 189652224, 189674200, 189700002, 189724047, 189800265, 189836314, 189909115, 189913959, 189943432, 189951463, 189951468 ];
+window.SCRIPTS = [ 188339330, 189724037, 188339296, 188339298, 188340596, 188351002, 188610540, 188610545, 188618901, 188619019, 188699334, 188860857, 188869861, 189021033, 189047757, 189279488, 189641079, 189652224, 189674200, 189700002, 189724047, 189800265, 189836314, 189909115, 189913959, 189943432, 189951463, 189951468, 190563612, 190563617, 190563618, 190563620 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -19,4 +19,5 @@ window.INPUT_SETTINGS = {
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
     {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/188339295/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/188339312/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/188339293/1/ammo.js', 'preload' : true},
+    {'moduleName' : 'effekseer_native', 'glueUrl' : 'files/assets/190563620/1/effekseer.js', 'wasmUrl' : 'files/assets/190563619/1/effekseer_native.wasm', 'fallbackUrl' : '', 'preload' : true},
 ];
